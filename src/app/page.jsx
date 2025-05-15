8464882364aa0fd09e27ee875a6116d54a180501
Seokjin Lee <seokjin.dev@gmail.com>
2025-05-15 16:24:34 +0900

    배너 슬라이드 컴포넌트 추가 및 스타일링: BannerSlide.jsx와 BannerSlide.module.css 파일을 생성하여 배너 슬라이드 기능을 구현하고, 슬라이드 데이터는 banners.json에서 불러오도록 설정함

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,6 @@
+import { BannerSlide } from "./BannerSlide"
 
 export default function Home() {
-  return (
-    <div>
-      <BannerSlide />
-    </div>
-  );
-}
-
-function BannerSlide() {
-  return (
-    <div>
-      <img src="data/banner/main_banner_qcjgcbw.png" />
-    </div>
-  )
+  const slides = require("../data/banners.json")
+  return <BannerSlide slides={slides} />
 }
