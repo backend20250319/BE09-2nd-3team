e8b995b470eb926f3eff295f0e6f1a2c4c9427f1
binipk <mingyoku93@gmail.com>
2025-05-16 12:15:35 +0900

    for pulling

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,3 +1,16 @@
 export default function Menu() {
+    return (
+        <div className="subTab">
+            <div className="inner">
+                <div className="tabLink">
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+
+                </div>
+            </div>
+        </div>
+    )
+
   
 }
